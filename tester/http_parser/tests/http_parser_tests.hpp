f3b8cb76cb79328d7ed5cfc927841af80f305c36
Ana Juncosa montes <ajm.zetazix@gmail.com>
2022-07-21 14:54:37 +0200

    modify: http_parser tests

--- tester/http_parser/tests/http_parser_tests.hpp
+++ tester/http_parser/tests/http_parser_tests.hpp
@@ -7,6 +7,7 @@ namespace ws_tester
 	
 bool http_parser_test1(size_t test_n, bool print);
 bool http_parser_test2(size_t test_n, bool print);
+bool http_parser_test3(size_t test_n, bool print);
 
 } // namespace ws_tester
 
