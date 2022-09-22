ec925cc6bfb19fa4f065ce5aae2f32317f3c0cd0
Ana Juncosa montes <ajm.zetazix@gmail.com>
2022-09-22 19:16:54 +0200

    fix: http_uri_parser: parsing query strings when path has url encoding

---
+++ www/formula1/eñe.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>EÑE</title>
+</head>
+<body>
+<h1>hola aquí hay caracteres raros como la eñe!</h1>
+<pre>
+<?php
+echo "<h2>¿hablas español?</h2>";
+echo "-----REQUEST-----\n";
+print_r($_REQUEST);
+echo "-----SERVER------\n";
+print_r($_SERVER);
+?>
+</pre>
+</body>
+</html>
+
