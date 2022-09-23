1f8575b02a80d9a56bc2bc8ccbbb15aace7fd8ea
Adrian Pavel <adrian199909@gmail.com>
2022-09-23 15:54:13 +0200

    Fix: Implemented new tester model

--- tester/test_utils.cpp
+++
@@ -1,8 +0,0 @@
-#include "tests_utils.hpp"
-
-namespace ws_tester
-{
-	void print_test_result(size_t test_n, bool result) {
-		std::cout << "Test " << test_n << ": " << (result ? "✅" : "❌") << std::endl;
-	}
-} // namespace ws
