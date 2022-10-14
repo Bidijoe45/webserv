493393cb69b732cb337c84e5000a2088dae7af57
Ana Juncosa <ajm.zetazix@gmail.com>
2022-10-14 16:53:52 +0200

    fix: content_length and its tester (stoul is c++11 so changed it for strtoul)

--- srcs/http/headers/http_header_content_length.cpp
+++ srcs/http/headers/http_header_content_length.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <stdexcept>
 #include <iostream>
+#include <cstdlib>
 
 #include "http_headers.hpp"
 #include "string_utils.hpp"
@@ -23,26 +24,13 @@ namespace ws
 
 	void HttpHeaderContentLength::parse_value()
 	{
-		if (is_string_digit(this->value, this->value.size()) == false)
+		if (this->value.size() == 0 || is_string_digit(this->value, this->value.size()) == false)
 		{
 			this->is_valid = false;
 			std::cout << "Content Length Header: invalid content-length chars" << std::endl;
 			return;
 		}
-		try
-		{
-			this->content_length = std::stoul(this->value);
-		}
-		catch (const std::out_of_range &e)
-		{
-			this->is_valid = false;
-			std::cout << "Content Length Header: " << e.what() << std::endl;
-		}
-		catch (const std::invalid_argument &e)
-		{
-			this->is_valid = false;
-			std::cout << "Content Length Header: " << e.what() << std::endl;
-		}
+		this->content_length = strtoul(this->value.c_str(), NULL, 10);
 	}
 
 	void HttpHeaderContentLength::set_value(const std::string &value)
